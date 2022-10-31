e38005a815f8b645b76348eb8a85b5f2d53467ac
ipoandres08 <73042619+ipoandres08@users.noreply.github.com>
2022-10-31 16:14:49 -0400

    2 files modified, 2 added

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
 import { CharactersComponent } from './characters/characters.component';
+import { AddCharacterComponent } from './add-character/add-character.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [MainPageComponent, CharactersComponent],
+  declarations: [MainPageComponent, CharactersComponent, AddCharacterComponent],
   exports: [MainPageComponent],
 })
 export class DbzModule {}
